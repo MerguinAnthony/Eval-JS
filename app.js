e01e6b6796b14c5ce34f0606ac30aef0e7b15d96
Anthony Merguin <anthonymerguin@MacBook-Air-de-Anthony.local>
2023-04-18 15:46:15 +0200

    modif js 18/04/2023 15:46

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Global variables
 const player1 = [(global = 0), (round = 0)];
 const player2 = [(global = 0), (round = 0)];
 
@@ -16,7 +15,7 @@ function defaultGame() {
   document.querySelector("#dice-img").setAttribute("src", "img/dices/de-1.png");
 }
 
-defaultGame();
+// new game
 
 const newGame = document.querySelector("#new-game-btn");
 if (newGame) {
@@ -25,8 +24,9 @@ if (newGame) {
   });
 }
 
-// . hold player 1
+defaultGame();
 
+// hold
 const hold = document.querySelector("#holdbtn");
 if (hold) {
   hold.addEventListener("click", () => {
@@ -47,7 +47,7 @@ if (hold) {
 // Roll dice player 1
 
 const dice = document.querySelector("#dicebtn");
-if (player1[0] > 1 || player2[0] <= 1 || hold) {
+if (player1[0] > 1 || player2[0] > 1 || hold) {
   if (dice) {
     dice.addEventListener("click", () => {
       const randomCalc = Math.floor(Math.random() * 6) + 1;
@@ -81,21 +81,16 @@ if (player1[0] > 1 || player2[0] <= 1 || hold) {
     });
   }
 }
-
-// Roll dice player 2
-
-if (player2[0] > 1 || player1[0] <= 1 || hold) {
-  if (dice) {
-    dice.addEventListener("click", () => {
-      const randomCalc = Math.floor(Math.random() * 6) + 1;
-      console.log(randomCalc);
-      if (randomCalc > 1) {
-        player2[0] += randomCalc;
-        document.querySelector("#score2").textContent = player2[0];
-      } else if (randomCalc === 1) {
-        player2[0] = 0;
-        document.querySelector("#score2").textContent = player2[0];
-      }
-    });
-  }
-}
+/*
+Le jeu s’initialise. 
+Le joueur 1 Lance le dé 
+le score du joueur 1 s’incrémente. 
+- si il clique sur hold, le score round du joueur 1 est mis à jour.
+- si il fait un 1, le score global du joueur 1 est mis à 0 et c’est au joueur 2 de jouer.
+Le joueur 2 lance le dé
+le score du joueur 2 s’incrémente.
+- si il clique sur hold, le score round du joueur 2 est mis à jour.
+- si il fait un 1, le score global du joueur 2 est mis à 0 et c’est au joueur 1 de jouer.
+Le joueur 1 lance le dé
+ainsi de suite jusqu’à ce que l’un des joueurs atteigne 100 points.
+*/
